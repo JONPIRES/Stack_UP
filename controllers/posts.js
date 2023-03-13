96c660dcaf76e7ee5b33f3c05ce12d62c612700b
JONPIRES <jonbpires@gmail.com>
2023-03-13 16:45:36 -0300

    create post route for Posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,1 +1,30 @@
 
+const express = require('express');
+
+const router = express.Router();
+
+const { Posts } = require('../models');
+
+
+router.get('/', async (req,res,next)=>{
+try{
+    const posts = await Posts.find({})
+    res.render('posts/index.ejs', )
+
+}catch(err){
+    console.log(err)
+    return next()
+}
+})
+
+router.post('/post', async (req,res,next)=>{
+    try{
+        const newPost = await Posts.create(req.body)
+        console.log (newPost)
+        res.redirect('/posts')
+    }catch(err){
+        console.log(err)
+        return next()
+    }
+})
+
