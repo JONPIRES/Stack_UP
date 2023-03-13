f3fd2be8e6a0e948744f09d5330220d1d0577c49
JONPIRES <jonbpires@gmail.com>
2023-03-13 19:18:08 -0300

    Create signin attempt route

--- controllers/users.js
+++ controllers/users.js
@@ -26,20 +26,20 @@ router.get('/logout', (req, res) => {
 
 router.post('/login', async(req, res, next) => {
     try {
-        const personTryingToLogInInfo = req.body;
-        const foundUser = await Users.findOne({username: personTryingToLogInInfo.username});
+        const loginAttempt = req.body;
+        const userFound = await Users.findOne({username: loginAttempt.username});
         // If they didn't provide a valid username that exists, throw them to the register so they can create an account
-        // if(!foundUser) return res.redirect('/signup');
+        // if(!userFound) return res.redirect('/signup');
         // bcrypt compare will always return a boolean. Its true if they are the same and false if they don't match
         // bcrypt knows that the first is supposed to be plain text and it's comparing if that's what given to them when they hashed the password originally.
-        const match = await bcrypt.compare(personTryingToLogInInfo.password, foundUser.password);
+        const match = await bcrypt.compare(loginAttempt.password, userFound.password);
         console.log(match);
         if(!match) return res.send("Email or password doesn't match!");
         req.session.currentUser = {
-            id: foundUser._id,
-            username: foundUser.username,
+            id: userFound._id,
+            username: userFound.username,
         };
-        return res.redirect('/musicians');
+        return res.redirect('/posts');
     } catch(err) {
         console.log(err);
         return next();
