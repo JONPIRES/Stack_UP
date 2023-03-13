5ba70a7102eaa3684809624a765488c4f50ec679
JONPIRES <jonbpires@gmail.com>
2023-03-13 15:01:03 -0300

    updated user model

--- models/Users.js
+++ models/Users.js
@@ -7,15 +7,26 @@ const userSchema = new mongoose.Schema(
         username: {
             type: String,
             required: [true, "Please provide a name"],
-            unique: [true, "Must be original. Apparently you're not"]
+            unique: [true, "Username already exists"]
         }, 
         password: {
             type: String,
-            required: [true, "Please provide the URL for an image"]
+            required: [true, "please provide a password"]
         }, 
         email: {
             type: String,
-            required: [true, "Please provide me with an email!"]
+            required: [true, "Please provide an email!"]
+        },
+        fName:{
+            type: String,
+            required: [true, "Please provide your first name"]
+        },
+        lName:{
+            type: String,
+            required: [true, "Please provide your first name"]
+        },
+        img: {
+            type:String
         }
     },
     {
