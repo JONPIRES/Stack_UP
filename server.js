3055bb804be830ef45134adcf5d5de0316b086e6
JONPIRES <jonbpires@gmail.com>
2023-03-13 11:24:10 -0300

    server.js set up

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const methodOverride = require('method-override');
+
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(methodOverride('_method'));
+
+
+app.get('/', (req,res) =>{
+    res.render('home')
+})
+
+
+
+app.get("/*" , (req,res) =>{
+    res.render("404");
+})
+
+app.listen(4000, function(){
+    console.log('I am listening on port 4000')
+})
