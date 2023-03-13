24995eae7e67053adcd7125a7b83d2e5fd8075b3
JONPIRES <jonbpires@gmail.com>
2023-03-13 18:44:27 -0300

    sign in route done

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 const {Users} = require('../models')
 
 router.get('/signup', (req,res)=>{
     res.render('user/signup.ejs')
 })
+
+router.get('/signin', (req, res) => {
+    res.render('user/signin');
+});
 
 
 router.post('/signUp', async (req,res,next)=>{
