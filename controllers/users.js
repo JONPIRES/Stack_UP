9e4bf7fba7b28794d5660c2d4b5da5ea41039cd1
JONPIRES <jonbpires@gmail.com>
2023-03-14 17:21:42 -0300

    add function so only if a user is logged in, he can see the posts page and create a req.session.user in users.js

--- controllers/users.js
+++ controllers/users.js
@@ -36,18 +36,16 @@ router.post('/signin', async(req, res, next) => {
     try {
         const loginAttempt = req.body;
         const userFound = await Users.findOne({username: loginAttempt.username});
-        // If they didn't provide a valid username that exists, throw them to the register so they can create an account
-        // if(!userFound) return res.redirect('/signup');
-        // bcrypt compare will always return a boolean. Its true if they are the same and false if they don't match
-        // bcrypt knows that the first is supposed to be plain text and it's comparing if that's what given to them when they hashed the password originally.
         const match = await bcrypt.compare(loginAttempt.password, userFound.password);
         console.log(match);
-        if(!match) return res.send("Email or password doesn't match!");
-        req.session.currentUser = {
-            id: userFound._id,
-            username: userFound.username,
-        };
-        return res.redirect('/posts');
+        // if statement only works if the username is correct, if the username does not exist, it thorws an error.
+        if(!match) return res.send("<h1>Email or password doesn't match!</h1>");
+            
+        delete userFound.password;
+        req.session.user = userFound;
+        console.log(req.session.user)
+        res.redirect('/posts');
+            
     } catch(err) {
         console.log(err);
         return next();
